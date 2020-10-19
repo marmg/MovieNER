355d2dfae0e1b3097cc284d8f6413574c03c8cee
marmg <702304@celes.unizar.es>
2020-10-19 14:23:29 +0200

    Fixed minor bugs

--- src/detector.py
+++ src/detector.py
@@ -20,20 +20,20 @@ logger.setLevel(logging.ERROR)
 # NER Models
 model = spacy.load("en_core_web_lg")
 ner = AlbertNER()
-ner.load("../assets/models/conll03")
+ner.load("assets/models/conll03")
 
 # QA Model to disambiguate
 qa = AlbertQA()
-qa.load("../assets/models/squad")
+qa.load("assets/models/squad")
 
 # List of genres, actor, directors and titles
-with open("../assets/genres.list", "r") as f:
+with open("assets/genres.list", "r") as f:
     genres = f.read().split("\n")    
-with open("../assets/titles.list", "r") as f:
+with open("assets/titles.list", "r") as f:
     titles = f.read().split("\n")
-with open("../assets/actors.list", "r") as f:
+with open("assets/actors.list", "r") as f:
     actors = f.read().split("\n")
-with open("../assets/directors.list", "r") as f:
+with open("assets/directors.list", "r") as f:
     directors = f.read().split("\n")
     
 # Regex
@@ -155,7 +155,7 @@ pat_awards = fr"\b(?:{'|'.join(awards_l)})\b"
 
 # Check data with movie database
 cols = ["original_title", "year", "genre", "director", "actors", "description"]
-df_movies = pd.read_csv("../assets/movies.csv")
+df_movies = pd.read_csv("assets/movies.csv")
 df_movies = df_movies.loc[df_movies.actors.notna()]
 
 def check_data(actors, directors, years, titles, genres):
@@ -456,7 +456,7 @@ def disambiguate_person(person, text):
     q_director = f"Is {person} a director?"
     q_actor = f"Is {person} an actor?"
 
-    a_actor = qa.answer(q_actor, text,  device='cuda', overwrite_cache=True)
+    a_actor = qa.answer(q_actor, text, overwrite_cache=True)
     
     if a_actor:
         if re.findall(pat_director, a_actor, re.IGNORECASE):
@@ -464,7 +464,7 @@ def disambiguate_person(person, text):
         else:
             return ACTOR
     else:
-        a_director = qa.answer(q_director, text, device='cuda', overwrite_cache=True)
+        a_director = qa.answer(q_director, text, overwrite_cache=True)
         if a_director:
             return DIRECTOR
         else:
@@ -492,7 +492,7 @@ def get_directors_from_df(text, directors):
 def get_entities(text):
     doc = model(text)
     entities_spacy = [(ent.text, ent.label_) for ent in doc.ents]
-    entities_albert = ner.extract(text, device='cuda', overwrite_cache=True)
+    entities_albert = ner.extract(text, overwrite_cache=True)
     
     return entities_spacy, entities_albert
 
